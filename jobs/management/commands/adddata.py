31db0b64f66c92b848d083e90b8aa92d62db2298
Spolox <draconvs@yandex.ru>
2020-11-21 23:37:00 +0300

    fixed some bugs

--- jobs/management/commands/adddata.py
+++ jobs/management/commands/adddata.py
@@ -8,7 +8,7 @@ from stepik_hh.settings import MEDIA_SPECIALITY_IMAGE_DIR, MEDIA_COMPANY_IMAGE_D
 
 class Command(BaseCommand):
     help = 'Write new data to database from <filename>.\n' \
-           'Filename must contain lists of jobs, companies, specialties'
+           'Filename should contain lists of jobs, companies, specialties'
 
     def add_arguments(self, parser):
         parser.add_argument('-f', '--filename', help="File with new data for database")
@@ -19,36 +19,33 @@ class Command(BaseCommand):
             module = __import__(filename[0])
         else:
             raise Exception('This is not python file. Use python file with data')
-        if 'companies' not in dir(module):
-            raise Exception('Not found companies in module')
-        if 'jobs' not in dir(module):
-            raise Exception('Not found jobs in module')
-        if 'specialties' not in dir(module):
-            raise Exception('Not found specialties in module')
-        for specialty in module.specialties:
-            if Specialty.objects.filter(code=specialty['code']).first() is None:
-                Specialty.objects.create(
-                    code=specialty['code'],
-                    title=specialty['title'],
-                    picture=os.path.join(MEDIA_SPECIALITY_IMAGE_DIR, 'specty_' + specialty['code'] + '.png'),
+        if 'specialties' in dir(module):
+            for specialty in module.specialties:
+                if Specialty.objects.filter(code=specialty['code']).first() is None:
+                    Specialty.objects.create(
+                        code=specialty['code'],
+                        title=specialty['title'],
+                        picture=os.path.join(MEDIA_SPECIALITY_IMAGE_DIR, 'specty_' + specialty['code'] + '.png'),
+                    )
+        if 'companies' in dir(module):
+            for company in module.companies:
+                Company.objects.create(
+                    name=company['title'],
+                    location=company['location'],
+                    description=company['description'],
+                    employee_count=company['employee_count'],
+                    logo=os.path.join(MEDIA_COMPANY_IMAGE_DIR, company['logo']),
                 )
-        for company in module.companies:
-            Company.objects.create(
-                name=company['title'],
-                location=company['location'],
-                description=company['description'],
-                employee_count=company['employee_count'],
-                logo=os.path.join(MEDIA_COMPANY_IMAGE_DIR, company['logo']),
-            )
-        for job in module.jobs:
-            Vacancy.objects.create(
-                title=job['title'],
-                specialty=Specialty.objects.get(code=job['specialty']),
-                company=Company.objects.get(pk=job['company']),
-                skills=job['skills'],
-                description=job['description'],
-                salary_min=job['salary_from'],
-                salary_max=job['salary_to'],
-                published_at=job['posted'],
-            )
+        if 'jobs' in dir(module):
+            for job in module.jobs:
+                Vacancy.objects.create(
+                    title=job['title'],
+                    specialty=Specialty.objects.get(code=job['specialty']),
+                    company=Company.objects.get(pk=job['company']),
+                    skills=job['skills'],
+                    description=job['description'],
+                    salary_min=job['salary_from'],
+                    salary_max=job['salary_to'],
+                    published_at=job['posted'],
+                )
         print('New data are added')
