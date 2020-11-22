973ec2837af7bf15d928e52f9b0230ece0cad185
Spolox <draconvs@yandex.ru>
2020-11-22 17:27:55 +0300

    refactoring

--- jobs/management/commands/import_data.py
+++ jobs/management/commands/import_data.py
@@ -1,12 +1,10 @@
 import os
 import sys
 
-from PIL import Image
-from django.core.management.base import BaseCommand
 from django.core.files import File
+from django.core.management.base import BaseCommand
 
 from jobs.models import Specialty, Company, Vacancy
-from stepik_hh.settings import MEDIA_SPECIALITY_IMAGE_DIR, MEDIA_COMPANY_IMAGE_DIR
 
 
 class Command(BaseCommand):
@@ -17,17 +15,17 @@ class Command(BaseCommand):
         parser.add_argument('-f', '--filename', help="File with new data for database")
 
     def handle(self, *args, **options):
-        full_path_file = options['filename']
-        directory = os.path.dirname(full_path_file)
+        file_full_path = options['filename']
+        directory = os.path.dirname(file_full_path)
         if directory:
             sys.path.append(directory)
-        (filename, ext) = os.path.splitext(os.path.basename(full_path_file))
+        (filename, ext) = os.path.splitext(os.path.basename(file_full_path))
         if ext == '.py':
-            module = __import__(filename)
+            module_data = __import__(filename)
         else:
             raise Exception('This is not python file. Use python file with data')
-        if 'specialties' in dir(module):
-            for specialty in module.specialties:
+        if 'specialties' in dir(module_data):
+            for specialty in module_data.specialties:
                 if Specialty.objects.filter(code=specialty['code']).first() is None:
                     specialty_img_filename = 'specty_' + specialty['code'] + '.png'
                     with open(os.path.join(directory,specialty_img_filename), 'rb') as specialty_img_file:
@@ -36,18 +34,18 @@ class Command(BaseCommand):
                             title=specialty['title'],
                             picture=File(specialty_img_file, specialty_img_filename),
                         )
-        if 'companies' in dir(module):
-            for company in module.companies:
-                with open(os.path.join(directory, company['logo']), 'rb') as company_img_file:
+        if 'companies' in dir(module_data):
+            for company in module_data.companies:
+                with open(os.path.join(directory, company['logo']), 'rb') as logo_img_file:
                     Company.objects.create(
                         name=company['title'],
                         location=company['location'],
                         description=company['description'],
                         employee_count=company['employee_count'],
-                        logo=File(company_img_file, company['logo']),
+                        logo=File(logo_img_file, company['logo']),
                     )
-        if 'jobs' in dir(module):
-            for job in module.jobs:
+        if 'jobs' in dir(module_data):
+            for job in module_data.jobs:
                 Vacancy.objects.create(
                     title=job['title'],
                     specialty=Specialty.objects.get(code=job['specialty']),
