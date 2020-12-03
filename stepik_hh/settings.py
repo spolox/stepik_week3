1637dc44ed54a849524369f86ce1ae85f19dbd9e
Spolox <draconvs@yandex.ru>
2020-12-03 03:09:13 +0300

    Fixed some problems.

--- stepik_hh/settings.py
+++ stepik_hh/settings.py
@@ -144,25 +144,3 @@ MESSAGE_TAGS = {
 }
 
 SEARCH_EXAMPLES = ['Python', 'Django', 'Angular', 'C++']
-
-LOGGING = {
-    'version': 1,
-    'filters': {
-        'require_debug_true': {
-            '()': 'django.utils.log.RequireDebugTrue',
-        },
-    },
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'filters': ['require_debug_true'],
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django.db.backends': {
-            'level': 'DEBUG',
-            'handlers': ['console'],
-        },
-    },
-}
